ad42a4821af7db9e5fd7e463974291f0fcf188dc
hella808 <krokodilsteam@mail.ru>
2025-10-16 15:24:42 +0500

    Add files via upload

---
+++ Calculate/Icalculator/Icalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate.ICalculate
+{
+    internal interface ICalculator
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int Addition(int num1, int num2y);
+        public int Subtraction(int num1, int num2);
+        public int Multiplication(int num1, int num2);
+        public int Division(int num1, int num2);
+    }
+}
---
+++ Calculate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Calculate;
+
+Calculating calculating = new Calculating();
+while (true)
+{
+    Console.WriteLine("Введите первое число");
+    string FirstInput = Console.ReadLine();
+    int num1 = int.Parse(FirstInput);
+    Console.WriteLine("Введите второе число");
+    string SecondInput = Console.ReadLine();
+    int num2 = int.Parse(SecondInput);
+    Console.WriteLine("Введите операцию (+,-,*,/)");
+    string Symbol = Console.ReadLine();
+    if (Symbol == "+")
+    {
+        Console.WriteLine(calculating.Addition(num1, num2));
+    }
+    else if (Symbol == "-")
+    {
+        Console.WriteLine(calculating.Subtraction(num1, num2));
+    }
+    else if (Symbol == "*")
+    {
+        Console.WriteLine(calculating.Multiplication(num1, num2));
+    }
+    else if (Symbol == "/")
+    {
+        Console.WriteLine(calculating.Division(num1, num2));
+    }
+    Console.WriteLine("Хотите ли вы завершить работу?");
+    string choose = Console.ReadLine();
+    if (choose == "да")
+    {
+        break;
+    }
+    else
+    {
+        continue;
+    }
+}
