b95aa9feec8a58410f38af9ad05816c4701c6724
hella808 <krokodilsteam@mail.ru>
2025-10-17 17:05:57 +0500

    Add files via upload

--- Calculate/Icalculator/Icalculator.cs
+++ Calculate/Icalculator/Icalculator.cs
@@ -10,7 +10,7 @@ namespace Calculate.ICalculate
     {
         public int num1 { get; set; }
         public int num2 { get; set; }
-        public int Addition(int num1, int num2y);
+        public int Addition(int num1, int num2);
         public int Subtraction(int num1, int num2);
         public int Multiplication(int num1, int num2);
         public int Division(int num1, int num2);
--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -2,6 +2,7 @@
 using Calculate;
 
 Calculating calculating = new Calculating();
+UpCalculating upcalculating = new UpCalculating();
 while (true)
 {
     Console.WriteLine("Введите первое число");
@@ -28,11 +29,53 @@ while (true)
     {
         Console.WriteLine(calculating.Division(num1, num2));
     }
+
     Console.WriteLine("Хотите ли вы завершить работу?");
     string choose = Console.ReadLine();
     if (choose == "да")
     {
-        break;
+        Console.WriteLine("Хотите ли вы попробовать другой калькулятор?");
+        string chose = Console.ReadLine();
+        if (chose == "да")
+        {
+            Console.WriteLine("Введите число:");
+            string Input = Console.ReadLine();
+            int num3 = int.Parse(Input);
+            Console.WriteLine("Выберите операцию(1 - возведение в квадрат, 2 - возведение в куб, 3 - корень числа, 4 - косинус, 5 - синус, 6 - тангенц, 7 - выход");
+            string Math = Console.ReadLine();
+            if (Math == "1")
+            {
+                Console.WriteLine(upcalculating.Exponentiation2(num3));
+            }
+            else if (Math == "2")
+            {
+                Console.WriteLine(upcalculating.Exponentiation3(num3));
+            }
+            else if (Math == "3")
+            {
+                Console.WriteLine(upcalculating.Root(num3));
+            }
+            else if (Math == "4")
+            {
+                Console.WriteLine(upcalculating.Cos(num3));
+            }
+            else if (Math == "5")
+            {
+                Console.WriteLine(upcalculating.Sin(num3));
+            }
+            else if (Math == "6")
+            {
+                Console.WriteLine(upcalculating.Tan(num3));
+            }
+            else if (Math == "7")
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
     }
     else
     {
