0e1aa94b353727f6be964738711856978836b621
hella808 <krokodilsteam@mail.ru>
2025-10-22 10:22:41 +0500

    Add files via upload

---
+++ Calculate/Calculate/Calculating.cs
@@ -0,0 +1,32 @@
+﻿using Calculate.ICalculate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    internal class Calculating: ICalculator
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
