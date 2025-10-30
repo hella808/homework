bb8706d861752ff150d3b9ed1e9f5861a9b41b48
hella808 <krokodilsteam@mail.ru>
2025-10-30 15:53:36 +0500

    Add files via upload

---
+++ WinForm/Program.cs
@@ -0,0 +1,12 @@
+namespace Win1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
