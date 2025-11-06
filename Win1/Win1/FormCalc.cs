ee40cf72cb05db2381c326e72db205bc21c6f64c
hella808 <krokodilsteam@mail.ru>
2025-11-06 14:59:28 +0500

    Add files via upload

---
+++ Win1/Win1/FormCalc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win1
+{
+    public partial class FormCalc : Form
+    {
+        public FormCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private double summ(double a, double b)
+        {
+            return a + b;
+        }
+        private void Summbtn_Click(object sender, EventArgs e)
+        {
+            num3txt.Text = Convert.ToString(summ(Convert.ToDouble(num1txt.Text), Convert.ToDouble(num2txt.Text)));
+        }
+        private double sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        private void Subbtn_Click(object sender, EventArgs e)
+        {
+            num3txt.Text = Convert.ToString(sub(Convert.ToDouble(num1txt.Text), Convert.ToDouble(num2txt.Text)));
+        }
+        private double mult(double a, double b)
+        {
+            return a * b;
+        }
+
+        private void Multibtn_Click(object sender, EventArgs e)
+        {
+            num3txt.Text = Convert.ToString(mult(Convert.ToDouble(num1txt.Text), Convert.ToDouble(num2txt.Text)));
+        }
+        private double div(double a, double b)
+        {
+            return a / b;
+        }
+        private void Divbtn_Click(object sender, EventArgs e)
+        {
+            num3txt.Text = Convert.ToString(div(Convert.ToDouble(num1txt.Text), Convert.ToDouble(num2txt.Text)));
+        }
+    }
+}
---
+++ Win1/Win1/FormWin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win1
+{
+    public partial class FormWin : Form
+    {
+        public FormWin()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCalc paint = new FormCalc();
+            paint.MdiParent = this;
+            paint.Show();
+        }
+
+        private void calcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCalc calc = new FormCalc();
+            calc.MdiParent = this;
+            calc.Show();
+        }
+    }
+}
